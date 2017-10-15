83dc135fd92fd9156eac415b9e095deb14d55b28
Jia Yi <apple@s-169-232-96-145.resnet.ucla.edu>
2017-10-15 15:34:59 -0700

    update create.sql with constraints and updated queries.sql

--- Project_1A/create.sql
+++ Project_1A/create.sql
@@ -1,90 +1,123 @@
 CREATE TABLE IF NOT EXISTS Movie (
-id INT PRIMARY KEY NOT NULL,
+id INT PRIMARY KEY NOT NULL, 
+/*Primary Key #1: Every movie should have a unique identification number.*/
 title VARCHAR(100) NOT NULL,
 year INT NOT NULL,
 rating VARCHAR(10) NOT NULL,
 company VARCHAR(50) NOT NULL
-);
+)ENGINE = INNODB;
 
 
 CREATE TABLE IF NOT EXISTS Actor (
 id INT PRIMARY KEY NOT NULL,
+/*Primary Key #2: Every actor should have a unique identification number.*/
 last VARCHAR(20) NOT NULL,
 first VARCHAR(20) NOT NULL,
 sex VARCHAR(6) NOT NULL,
 dob DATE NOT NULL,
-dod DATE
-);
+dod DATE,
+CHECK (dob <= dod)
+/*CHECK #1: Date of birth should be smaller than date of death*/
+)ENGINE = INNODB;
+
 
 
 CREATE TABLE IF NOT EXISTS Sales (
-mid INT NOT NULL,
+mid INT PRIMARY KEY NOT NULL,
+/*Primary Key #3: Every movie should have a unique record of sales.*/
 ticketsSold INT NOT NULL,
 totalIncome INT NOT NULL,
-FOREIGN KEY fk_mid(mid)
+FOREIGN KEY fk_mid(mid) 
 REFERENCES Movie(id)
 ON UPDATE CASCADE
-ON DELETE CASCADE
-);
+ON DELETE CASCADE,
+/* Referential Integrity #1: Movie id should exist in table Movie in order to have sales */
+CHECK (ticketsSold >= 0)
+/*Check #2: Tickets Sold should be non negative.*/
+)ENGINE = INNODB;
+
 
 
 CREATE TABLE IF NOT EXISTS Director (
 id INT PRIMARY KEY NOT NULL,
+/*Primary Key #4: Every director should have a unique identification number.*/
 last VARCHAR(20) NOT NULL,
 first VARCHAR(20) NOT NULL,
 dob DATE NOT NULL,
-dod DATE
-);
+dod DATE,
+CHECK (dob <= dod)
+/*CHECK #3: Date of birth should be smaller than date of death*/
+)ENGINE = INNODB;
+
 
 
 CREATE TABLE IF NOT EXISTS MovieGenre (
 mid INT NOT NULL,
 genre VARCHAR(20) NOT NULL,
+PRIMARY KEY(mid,genre),
+/*Primary Key #5: Every mid+genre of a movie should be unique.*/
 FOREIGN KEY fk_mid(mid)
 REFERENCES Movie(id)
 ON UPDATE CASCADE
 ON DELETE CASCADE
-);
+/* Referential Integrity #2: Movie id should exist in table Movie in order to have MovieGenre*/
+)ENGINE = INNODB;
+
 
 
 CREATE TABLE IF NOT EXISTS MovieDirector (
 mid INT NOT NULL, 
 did INT NOT NULL,
+PRIMARY KEY(mid,did),
+/*Primary Key #6: Every mid and did pair in MovieDirector should be unique.*/
 FOREIGN KEY fk_mid(mid)
 REFERENCES Movie(id)
 ON UPDATE CASCADE
 ON DELETE CASCADE,
+/* Referential Integrity #3: Movie id should exist in table Movie in order to have mid in MovieDirector.*/
 FOREIGN KEY fk_did(did)
 REFERENCES Director(id)
 ON UPDATE CASCADE
 ON DELETE CASCADE
-);
+/* Referential Integrity #4: Director id should exist in table Director in order to have did in MovieDirector.*/
+)ENGINE = INNODB;
+
 
 
 CREATE TABLE IF NOT EXISTS MovieActor (
 mid INT NOT NULL, 
 aid INT NOT NULL, 
 role VARCHAR(50) NOT NULL,
+PRIMARY KEY(mid,aid),
+/* Primary Key #7: Every mid and aid pair in MovieActor should be unique.*/
 FOREIGN KEY fk_mid(mid)
 REFERENCES Movie(id)
 ON UPDATE CASCADE
 ON DELETE CASCADE,
+/* Referential Integrity #5: Movie id should exist in table Movie in order to have mid in MovieActor.*/
 FOREIGN KEY fk_aid(aid)
 REFERENCES Actor(id)
 ON UPDATE CASCADE
 ON DELETE CASCADE
-);
+/* Referential Integrity #6: Actor id should exist in table Actor in order to have did in MovieActor.*/
+)ENGINE = INNODB;
+
 
 
 CREATE TABLE IF NOT EXISTS MovieRating (
-mid INT NOT NULL, 
+mid INT PRIMARY KEY NOT NULL, 
+/* Primary Key #8: Every MovieRating should have a unique mid.*/
 imdb INT NOT NULL, 
 rot INT NOT NULL,
 FOREIGN KEY fk_mid(mid)
 REFERENCES Movie(id)
 ON UPDATE CASCADE
-ON DELETE CASCADE
-);
+ON DELETE CASCADE,
+/* Referential Integrity #7: Movie id should exist in table Movie in order to have mid in MovieRating.*/
+CHECK (imdb >= 0 and imdb <= 100 and rot >= 0 and rot <= 100)
+/* Check #4: movie score should be among 0 to 100.*/
+)ENGINE = INNODB;
+
 
 
 CREATE TABLE IF NOT EXISTS Review (
@@ -93,37 +126,26 @@ time TIMESTAMP  NOT NULL,
 mid INT  NOT NULL,
 rating INT  NOT NULL,
 comment VARCHAR(500)  NOT NULL,
-PRIMARY KEY(name,time,mid)
-);
+PRIMARY KEY(name,time,mid),
+/*Primary Key #9: Every reviewer's name, time and movie id are unique in the table.*/
+FOREIGN KEY fk_mid(mid)
+REFERENCES Movie(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+/* Referential Integrity #8: Movie id should exist in table Movie in order to have mid in Review.*/
+)ENGINE = INNODB;
 
 
-CREATE TABLE IF NOT EXISTS MaxPersonID(
-id INT PRIMARY KEY NOT NULL
-);
+
+CREATE TABLE IF NOT EXISTS MaxPersonID (
+id INT NOT NULL
+)ENGINE = INNODB;
 
 
-CREATE TABLE IF NOT EXISTS MaxMovieID(
-id INT PRIMARY KEY NOT NULL
-);
+
+CREATE TABLE IF NOT EXISTS MaxMovieID (
+id INT NOT NULL
+)ENGINE = INNODB;
 
 
-CREATE TRIGGER tg_actor_maxpersonid
-AFTER INSERT ON Actor
-FOR EACH ROW 
-  	UPDATE MaxPersonID 
-  		SET id=IF(id < NEW.id, NEW.id, id);
-  
-
-CREATE TRIGGER tg_director_maxpersonid
-AFTER INSERT ON Director
-FOR EACH ROW
-  	UPDATE MaxPersonID 
-  		SET id=IF(id < NEW.id, NEW.id, id);
-
-
-CREATE TRIGGER tg_movie_maxmid
-AFTER INSERT ON Movie
-FOR EACH ROW
-  UPDATE MaxMovieID
-     SET id=(SELECT max(id) FROM Movie);
 
--- Project_1A/queries.sql
+++ Project_1A/queries.sql
@@ -8,9 +8,28 @@ where MovieActor.aid = Actor.id
 		and Movie.title = 'Death to Smoochy';
 
 
+--queries 2
+--Give me the count of all the directors who directed at least 4 movies.
+
+SELECT COUNT(*)
+FROM (SELECT did, COUNT(mid) as Mnum
+	  FROM MovieDirector
+	  GROUP BY did) as DirectorMNUM
+WHERE DirectorMNUM.Mnum>=4;
+
+
 --queries 3 
---give the name of actor born after 2000
+--Give me the name of actor born after 2000.
 
 SELECT distinct concat("<",first,"> <",last,">")
 FROM Actor 
 WHERE Actor.dob>20000000;
+
+
+--queries 4
+--Give me the names of movies that have rating higher than 85 on imdb.
+
+SELECT m.title 
+FROM Movie m, MovieRating r
+WHERE m.id=r.mid and r.imdb>85;
+
